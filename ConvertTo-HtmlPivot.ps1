a2d7ee9f133a50e08a7c90aeb470e25c86cc52b2
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-02-18 12:43:07 -0500

    Update ConvertTo-HtmlPivot.ps1

--- ConvertTo-HtmlPivot.ps1
+++ ConvertTo-HtmlPivot.ps1
@@ -6,20 +6,33 @@ function ConvertTo-HtmlPivot {
         Convert Hashtable structure to vertical HTML table format. Yes, I said it twice
     .PARAMETER InputObject
         Hashtable object which will be converted to a vertical table format
+    .PARAMETER DivID
+        Optional Div style identifier
     .EXAMPLE 
         Get-WmiObject -Class Win32_OperatingSystem | 
             Select Caption,BuildNumber,Version,SystemDirectory,RegisteredUser,InstallDate | 
                 ConvertTo-HtmlPivot
+    .EXAMPLE
+        Get-WmiObject -Class Win32_OperatingSystem | 
+            Select Caption,BuildNumber,Version,SystemDirectory,RegisteredUser,InstallDate | 
+                ConvertTo-HtmlPivot -DivID "table3"
     #>
     [CmdletBinding()]
     param (
         [parameter(Mandatory=$True, ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$True)]
         [ValidateNotNull()]
-        $InputObject
+        $InputObject,
+        [parameter(Mandatory=$False)]
+        [string] $DivID = ""
     )
     try {
         $columns = $InputObject.psobject.Properties.Name
-        $output = "<table>"
+        if ($DivID -ne "") {
+            $output = "<table div=`"$DivID`">"
+        }
+        else {
+            $output = "<table>"
+        }
         foreach ($col in $columns) {
             $val = $InputObject.psobject.Properties.Item($col).Value 
             $output += "<tr><td>$col</td><td>$val</td></tr>"
