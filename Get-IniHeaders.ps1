5d1535b66f60e9ed4adb3507e43d929d1356cc63
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2024-08-22 23:20:28 -0400

    formatting cleanup

--- Get-IniHeaders.ps1
+++ Get-IniHeaders.ps1
@@ -1,15 +1,14 @@
 function Get-IniHeaders {
-    param (
-        [parameter(Mandatory=$True, ValueFromPipeline = $True, HelpMessage = "Text Stream Value")]
-        [ValidateNotNullOrEmpty()]
-        [string] $Content,
-        [parameter(Mandatory=$False, HelpMessage = "Remove brackets within returned values")]
-        [switch] $NoBrackets
-    )
-    if (!$NoBrackets) {
-        $Content.Split([Environment]::NewLine) | Where {$_.StartsWith('[')}
-    }
-    else {
-        $Content.Split([Environment]::NewLine) | Where {$_.StartsWith('[')} | Foreach {($_.Replace('[','')).Replace(']','')}
-    }
+	param (
+		[parameter(Mandatory = $True, ValueFromPipeline = $True, HelpMessage = "Text Stream Value")]
+		[ValidateNotNullOrEmpty()]
+		[string] $Content,
+		[parameter(Mandatory = $False, HelpMessage = "Remove brackets within returned values")]
+		[switch] $NoBrackets
+	)
+	if (!$NoBrackets) {
+		$Content.Split([Environment]::NewLine) | Where-Object { $_.StartsWith('[') }
+	} else {
+		$Content.Split([Environment]::NewLine) | Where-Object { $_.StartsWith('[') } | ForEach-Object { ($_.Replace('[', '')).Replace(']', '') }
+	}
 }
